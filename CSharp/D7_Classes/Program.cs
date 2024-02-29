3e457749ec531c2dfff5e898f4c84169ee7ee4af
BusraYaydemir <147690122+BusraYaydemir@users.noreply.github.com>
2024-02-29 23:30:14 +0300

    Demo 7

---
+++ CSharp/D7_Classes/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D7_Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Create();
+            customerManager.Read();
+            customerManager.Update();
+            customerManager.Delete();
+
+            Console.WriteLine("\n\n-------------------\n\n");
+
+            ProductManager productManager = new ProductManager();
+            productManager.Create();
+            productManager.Read();
+            productManager.Update();
+            productManager.Delete();
+
+            Console.ReadLine();
+        }
+    }
+
+    public class CustomerManager
+    {
+        public void Create()
+        {
+            Console.WriteLine("Customer Created!");
+        }
+
+        public void Read()
+        {
+            Console.WriteLine("Customer Read!");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Customer Updated!");
+        }
+
+        public void Delete()
+        {
+            Console.WriteLine("Customer Deleted!");
+        }
+    }
+
+
+    public class ProductManager
+    {
+        public void Create()
+        {
+            Console.WriteLine("Product Created!");
+        }
+
+        public void Read()
+        {
+            Console.WriteLine("Product Read!");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Product Updated!");
+        }
+
+        public void Delete()
+        {
+            Console.WriteLine("Product Deleted!");
+        }
+    }
+}
