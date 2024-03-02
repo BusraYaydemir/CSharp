3c2a2c9c60f9ee6c048a502c220e9bb88b0dfa89
BusraYaydemir <147690122+BusraYaydemir@users.noreply.github.com>
2024-03-02 21:05:14 +0300

    Demo 9

---
+++ CSharp/D9_Interfaces/ICustomerDal.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D9_Interfaces
+{
+    interface ICustomerDal
+    {
+        void Add();
+        void Update();
+        void Delete();
+    }
+
+    class SqlServerCustomerDal : ICustomerDal
+    {
+        public void Add()
+        {
+            Console.WriteLine("SQLServer Added!");
+        }
+
+        public void Delete()
+        {
+            Console.WriteLine("SQLServer Deleted!");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("SQLServer Updated!");
+        }
+    }
+
+    class OracleCustomerDal : ICustomerDal
+    {
+        public void Add()
+        {
+            Console.WriteLine("Oracle Added!");
+        }
+
+        public void Delete()
+        {
+            Console.WriteLine("Oracle Deleted!");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Oracle Updated!");
+        }
+    }
+
+    class CustomerManager
+    {
+        public void Add(ICustomerDal customerDal)
+        {
+            customerDal.Add();
+        }
+
+        public void Update(ICustomerDal customerDal)
+        {
+            customerDal.Update();
+        }
+
+        public void Delete(ICustomerDal customerDal)
+        {
+            customerDal.Delete();
+        }
+    }
+}
---
+++ CSharp/D9_Interfaces/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D9_Interfaces
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Demo1();
+
+
+            //Demo2();
+
+            ICustomerDal[] customerDals = new ICustomerDal[2]
+            {
+                new SqlServerCustomerDal(),
+                new OracleCustomerDal()
+            };
+
+            foreach (var customerDal in customerDals)
+            {
+                customerDal.Add();
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void Demo2()
+        {
+            CustomerManager customerManager = new CustomerManager();
+
+            customerManager.Add(new SqlServerCustomerDal());
+            customerManager.Add(new OracleCustomerDal());
+        }
+
+        private static void Demo1()
+        {
+            PersonManager personManager = new PersonManager();
+
+            // personManager for Customer:
+            personManager.Add(new Customer() { Id = 1, FirstName = "FirstName 1", LastName = "LastName 2", Address = "Address" });
+
+            // personManager for Student:
+            Student student = new Student()
+            {
+                Id = 2,
+                FirstName = "FirstName 2",
+                LastName = "LastName 2",
+                Department = "Department"
+            };
+
+            personManager.Add(student);
+        }
+    }
+
+    interface IPerson
+    {
+        int Id { get; set; }
+        string FirstName { get; set; }
+        string LastName { get; set; }
+    }
+
+    class Customer : IPerson
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string Address { get; set; }
+    }
+
+    class Student : IPerson
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string Department { get; set; }
+    }
+
+    class PersonManager
+    {
+        public void Add(IPerson person)
+        {
+            Console.WriteLine(person.FirstName + " is Added!");
+        }
+    }
+}
