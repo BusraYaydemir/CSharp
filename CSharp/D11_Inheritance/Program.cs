614a20579b808855e71d3ed21d26e66ab260c9dd
BusraYaydemir <147690122+BusraYaydemir@users.noreply.github.com>
2024-03-03 01:13:49 +0300

    Demo 11

---
+++ CSharp/D11_Inheritance/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D11_Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] people = new Person[2]
+            {
+                new Person() {FirstName = "Person FirstName"}, new Customer() {FirstName = "Customer FirstName"}
+            };
+        }
+    }
+
+
+    class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+    }
+
+    class Customer : Person
+    {
+
+    }
+}
