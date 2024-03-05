b9f49273ed8343045e42c3a8d9d05dc14c6fe6df
BusraYaydemir <147690122+BusraYaydemir@users.noreply.github.com>
2024-03-06 01:16:21 +0300

    Demo 14

---
+++ CSharp/D14_RecapDemo1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D14_RecapDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.logger = new JsonLogger();
+            customerManager.Add();
+
+            Console.ReadLine();
+        }
+    }
+
+    class CustomerManager
+    {
+        public Logger logger;
+        public void Add()
+        {
+            logger.Log();
+            Console.WriteLine("Customer Added!");
+        }
+    }
+
+
+    interface Logger
+    {
+        void Log();
+    }
+
+    class DatabaseLogger : Logger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged to Database!");
+        }
+    }
+
+    class SmsLogger : Logger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged to Sms!");
+        }
+    }
+
+    class JsonLogger : Logger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged to Json!");
+        }
+    }
+}
