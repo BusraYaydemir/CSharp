16fc70d681d2eae904363f8f27ef947b93ef9449
BusraYaydemir <147690122+BusraYaydemir@users.noreply.github.com>
2024-03-29 02:10:02 +0300

    Demo 20

---
+++ CSharp/D20_Generics/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D20_Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Utilities utilities = new Utilities();
+            List<Product> products = utilities.BuildList<Product>(new Product { Name = "Tablet"}, new Product { Name = "Laptop"});
+
+            foreach (var product in products)
+            {
+                Console.WriteLine(product.Name);
+            }
+
+            List<Customer> customers = utilities.BuildList<Customer>(new Customer { FirstName = "Name1" }, new Customer { FirstName = "Name2" });
+
+            foreach (var customer in customers)
+            {
+                Console.WriteLine(customer.FirstName);
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+
+    class Utilities
+    {
+        public List<T> BuildList<T> (params T[] values)
+        {
+            return new List<T> (values);
+        }
+    }
+
+
+    class Product
+    {
+        public string Name;
+    }
+
+    interface IProductDal : IReporstory<Product>
+    {
+        /*
+        List<Product> GetAll();
+        Product Get();
+        void Add(Product product);
+        void Update(Product product);
+        void Delete(Product product);
+        */
+    }
+
+    class ProductDal : IProductDal
+    {
+        public void Add(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class Customer
+    {
+        public string FirstName;
+    }
+
+
+    interface ICustomerDal : IReporstory<Customer>
+    {
+        /*
+        List<Customer> GetAll();
+        Customer Get();
+        void Add(Customer customer);
+        void Update(Customer customer);
+        void Delete(Customer customer);
+        */
+    }
+
+
+    class CustomerDal : ICustomerDal
+    {
+        public void Add(Customer entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Customer entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Customer Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Customer> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Customer entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    interface IReporstory<T>
+    {
+        List<T> GetAll();
+        T Get();
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+
+}
