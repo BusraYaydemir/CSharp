370570ebe2b9e87056a9ba37dd64b21382e831ff
BusraYaydemir <147690122+BusraYaydemir@users.noreply.github.com>
2024-09-01 01:24:58 +0300

    .

---
+++ CSharp/D25_Events/Product.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D25_Events
+{
+
+    public delegate void StockControl();
+    public class Product
+    {
+        public event StockControl StockControlEvent;
+
+        private string _name;
+        private int _stock;
+
+        public Product(string name, int stock)
+        {
+            _name = name;
+            _stock = stock;
+        }
+
+        public string Name { get { return _name; } set { _name = value; } }
+        public int Stock { 
+            get { 
+                return _stock; 
+            } set {
+                _stock = value;
+                if (_stock <= 15 && StockControlEvent != null)
+                {
+                    StockControlEvent();
+                }
+            } 
+        }
+
+        public void Sell(int amount)
+        {
+            Stock -= amount;
+            Console.WriteLine("{0} stock : {1}", Name, Stock);
+        }
+    }
+}
---
+++ CSharp/D25_Events/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace D25_Events
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product hardDisk = new Product("Hard Disk", 50);
+            Product gsm = new Product("GSM", 50);
+            gsm.StockControlEvent += Gsm_StockControlEvent;
+
+            while (hardDisk.Stock > 0 && gsm.Stock > 0)
+            {
+                hardDisk.Sell(5);
+                gsm.Sell(5);
+                Thread.Sleep(1000);
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void Gsm_StockControlEvent()
+        {
+            Console.WriteLine("Gsm is about the finish!");
+        }
+    }
+}
