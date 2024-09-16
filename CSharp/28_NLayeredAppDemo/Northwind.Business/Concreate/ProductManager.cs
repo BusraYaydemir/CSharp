591efdbd76a42027bca2a19cfadd57c3bcafa431
BusraYaydemir <147690122+BusraYaydemir@users.noreply.github.com>
2024-09-16 19:34:53 +0300

    .

---
+++ CSharp/28_NLayeredAppDemo/Northwind.Business/Concreate/ProductManager.cs
@@ -0,0 +1,50 @@
+﻿using Northwind.Entities.Concreate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Northwind.DataAccess.Concreate;
+
+
+namespace Northwind.Business.Concreate
+{
+    public class ProductManager
+    {
+        ProductDal _productDal = new ProductDal();
+        public List<Product> GetAll()
+        {
+            // Business Code : Bu methoda erişme yetkin var mı?
+            
+            return _productDal.GetAll();
+        }
+
+        public Product All(int id)
+        {
+            // Business Code : Bu methoda erişme yetkin var mı?
+
+            return _productDal.Get(id);
+        }
+
+        public void Add(Product product)
+        {
+            // Business Code : Bu methoda erişme yetkin var mı?
+
+            _productDal.Add(product);
+        }
+
+        public void Update(Product product)
+        {
+            // Business Code : Bu methoda erişme yetkin var mı?
+
+            _productDal.Update(product);
+        }
+
+        public void Delete(Product product)
+        {
+            // Business Code : Bu methoda erişme yetkin var mı?
+
+            _productDal.Delete(product);
+        }
+    }
+}
---
+++ CSharp/28_NLayeredAppDemo/Northwind.DataAccess/Concreate/NorthwindContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Protocols;
+using Northwind.Entities.Concreate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Northwind.DataAccess.Concreate
+{
+    public class NorthwindContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"data source=(localdb)\mssqllocaldb;initial catalog=Northwind;integrated security=true");
+        }
+    }
+}
